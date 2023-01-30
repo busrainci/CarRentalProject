7bf8763a039b6c4688fffe46447a3b430c3b51c9
busrainci <inci_busra@hotmail.com>
2023-01-31 02:00:48 +0300

    10.Ders 1.Ödev tamamlandı

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -38,60 +38,74 @@ namespace Business.Concrete
 
         public IResult Delete(Car car)
         {
-            _carDal.Delete(car);
-            Console.WriteLine("{0} isimli araba listeden kaldırılmıştır.",car.CarName);
+            if (car.Description.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Delete(car);
+                Console.WriteLine(Messages.CarDeleted);
+            }
+            else
+            {
+                Console.WriteLine(Messages.CarNotDeleted);
+            }
+            return new ErrorDataResult<Car>(Messages.CarDeleted);
         }
 
-        public List<Car> GetAll()
+        public IDataResult<List<Car>> GetAll()
         {
-            return _carDal.GetAll();
+            if (DateTime.Now.Hour == 14)
+            {
+                return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.CarsListed);
         }
-        public List<Car> GetByDailyPrice(decimal min, decimal max)
+        public IDataResult<List<Car>> GetByDailyPrice(decimal min, decimal max)
         {
-            return _carDal.GetAll(c => c.DailyPrice >= min && c.DailyPrice <= max);
-        }
-
-        public Car GetById(int id)
-        {
-            return _carDal.Get(c => c.CarId == id);
-        }
-
-        public List<Car> GetCarByBrandId(int brandId)
-        {
-            return _carDal.GetAll(c => c.BrandId == brandId);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.DailyPrice >= min && c.DailyPrice <= max));
         }
 
-        public List<CarDetailDto> GetCarDetails()
+        public IDataResult<Car>GetById(int id)
         {
-            return _carDal.GetCarDetails();
+            return new SuccessDataResult<Car>(_carDal.Get(c => c.CarId == id));
         }
 
-        public List<CarDetailDto> GetCarDetailsDtos()
+        public IDataResult<List<Car>>GetCarByBrandId(int brandId)
         {
-            return _carDal.GetCarDetails();
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == brandId));
         }
 
-        public List<Car> GetCarsByBrandId(int brandId)
+        public IDataResult<List<CarDetailDto>> GetCarDetails()
         {
-            return _carDal.GetAll(c => c.BrandId == brandId);
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
         }
 
-        public List<Car> GetCarsByColorId(int colorId)
+        public IDataResult<List<CarDetailDto>> GetCarDetailsDtos()
         {
-            return _carDal.GetAll(c => c.ColorId == colorId);
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
         }
 
-        public void Update(Car car)
+        public IDataResult<List<Car>> GetCarsByBrandId(int brandId)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == brandId));
+        }
+
+        public IDataResult<List<Car>>GetCarsByColorId(int colorId)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == colorId));
+        }
+
+        public IResult Update(Car car)
         {
             if (car.Description.Length >=2 && car.DailyPrice>0)
             {
-                Console.WriteLine("{0} isimli araba güncellenmiştir.",car.CarName);
+                Console.WriteLine(car.CarName,Messages.CarUpdate);
 
             }
             else
             {
-                Console.WriteLine("{0} isimli araba güncellenemedi! lütfen arabann ismini ve günlük fiyatını kontrol ediniz.",car.CarName);
+                Console.WriteLine(car.CarName,Messages.CarNotUpdate);
             }
+            _carDal.Update(car);
+            return new ErrorResult(Messages.CarNotUpdate);
         }
          
     }
